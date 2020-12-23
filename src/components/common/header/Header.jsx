602cb29cf7be3325a4d4ba0581842f5da4a47b33
Gio Gastaldo <giogastaldo0601@gmail.com>
2020-12-23 12:20:01 -0500

    creating components

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header () {
+
+    return (
+        <section className="header">
+            <section className="header-logo">
+
+            </section>
+            <section className="header-navbar">
+
+            </section>
+        </section>
+    )
+
+}
+
+
+export default Header;
